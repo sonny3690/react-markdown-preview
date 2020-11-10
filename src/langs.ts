7c041ccbb2e4a9d3ef46a7954bbaa282315025a5
Sonny Mo <sonny3690@gmail.com>
2020-11-09 22:56:04 -0500

    report errors and use yarn

--- src/langs.ts
+++ src/langs.ts
@@ -1,4 +1,3 @@
-
 export interface ILangData {
   [key: string]: {
     /**
@@ -17,81 +16,90 @@ export interface ILangData {
      * file does not exist
      */
     e?: boolean;
-  }
+  };
 }
 
 const langData: ILangData = {
   meta: {},
-  markup: { a: ['html', 'xml', 'svg', 'mathml'] },
-  css: { p: 'markup' },
+  markup: { a: ["html", "xml", "svg", "mathml"] },
+  css: { p: "markup" },
   clike: {},
-  javascript: { p: 'markup', r: 'clike', a: 'js' },
+  javascript: { p: "markup", r: "clike", a: "js" },
   abap: {},
   abnf: {},
-  actionscript: { p: 'markup', r: 'javascript' },
+  actionscript: { p: "markup", r: "javascript" },
   ada: {},
   apacheconf: {},
   apl: {},
   applescript: {},
-  arduino: { r: 'cpp' },
+  arduino: { r: "cpp" },
   arff: {},
-  asciidoc: { a: 'adoc' },
+  asciidoc: { a: "adoc" },
   asm6502: {},
-  aspnet: { r: ['markup', 'csharp'] },
+  aspnet: { r: ["markup", "csharp"] },
   autohotkey: {},
   autoit: {},
-  bash: { a: 'shell' },
+  bash: { a: "shell" },
   basic: {},
   batch: {},
-  bison: { r: 'c' },
-  bnf: { a: 'rbnf' },
+  bison: { r: "c" },
+  bnf: { a: "rbnf" },
   brainfuck: {},
   bro: {},
-  c: { r: 'clike' },
-  csharp: { r: 'clike', a: ['cs', 'dotnet'] },
-  cpp: { r: 'c' },
+  c: { r: "clike" },
+  csharp: { r: "clike", a: ["cs", "dotnet"] },
+  cpp: { r: "c" },
   cil: {},
-  coffeescript: { r: 'javascript', a: 'coffee' },
+  coffeescript: { r: "javascript", a: "coffee" },
   cmake: {},
   clojure: {},
-  crystal: { r: 'ruby' },
+  crystal: { r: "ruby" },
   csp: {},
-  'css-extras': { r: 'css' },
-  d: { r: 'clike' },
-  dart: { r: 'clike' },
+  "css-extras": { r: "css" },
+  d: { r: "clike" },
+  dart: { r: "clike" },
   diff: {},
-  django: { r: 'markup-templating', a: 'jinja2' },
-  'dns-zone-file': { a: 'dns-zone' },
-  docker: { a: 'dockerfile' },
+  django: { r: "markup-templating", a: "jinja2" },
+  "dns-zone-file": { a: "dns-zone" },
+  docker: { a: "dockerfile" },
   ebnf: {},
   eiffel: {},
-  ejs: { r: ['javascript', 'markup-templating'] },
+  ejs: { r: ["javascript", "markup-templating"] },
   elixir: {},
   elm: {},
-  erb: { r: ['ruby', 'markup-templating'] },
+  erb: { r: ["ruby", "markup-templating"] },
   erlang: {},
-  fsharp: { r: 'clike' },
-  flow: { r: 'javascript' },
+  fsharp: { r: "clike" },
+  flow: { r: "javascript" },
   fortran: {},
   gcode: {},
   gedcom: {},
   gherkin: {},
   git: {},
-  glsl: { r: 'clike' },
-  gml: { r: 'clike', a: 'gamemakerlanguage' },
-  go: { r: 'clike' },
+  glsl: { r: "clike" },
+  gml: { r: "clike", a: "gamemakerlanguage" },
+  go: { r: "clike" },
   graphql: {},
-  groovy: { r: 'clike' },
-  haml:
-  {
-    p: ['css', 'coffeescript', 'erb', 'javascript', 'less', 'markdown', 'ruby', 'scss', 'textile'],
-    r: 'ruby'
+  groovy: { r: "clike" },
+  haml: {
+    p: [
+      "css",
+      "coffeescript",
+      "erb",
+      "javascript",
+      "less",
+      "markdown",
+      "ruby",
+      "scss",
+      "textile",
+    ],
+    r: "ruby",
   },
-  handlebars: { r: 'markup-templating' },
-  haskell: { a: 'hs' },
-  haxe: { r: 'clike' },
+  handlebars: { r: "markup-templating" },
+  haskell: { a: "hs" },
+  haxe: { r: "clike" },
   hcl: {},
-  http: { p: ['javascript', 'markup'] },
+  http: { p: ["javascript", "markup"] },
   hpkp: {},
   hsts: {},
   ichigojam: {},
@@ -100,136 +108,150 @@ const langData: ILangData = {
   ini: {},
   io: {},
   j: {},
-  java: { r: 'clike' },
-  javadoc: { p: ['scala'], r: ['markup', 'java', 'javadoclike'] },
-  javadoclike: { p: ['java', 'javascript', 'php'] },
+  java: { r: "clike" },
+  javadoc: { p: ["scala"], r: ["markup", "java", "javadoclike"] },
+  javadoclike: { p: ["java", "javascript", "php"] },
   javastacktrace: {},
-  jolie: { r: 'clike' },
+  jolie: { r: "clike" },
   jq: {},
-  jsdoc:
-  {
-    p: ['actionscript', 'coffeescript'],
-    r: ['javascript', 'javadoclike']
+  jsdoc: {
+    p: ["actionscript", "coffeescript"],
+    r: ["javascript", "javadoclike"],
   },
-  'js-extras':
-  {
-    p: ['actionscript', 'coffeescript', 'flow', 'n4js', 'typescript'],
-    r: 'javascript'
+  "js-extras": {
+    p: ["actionscript", "coffeescript", "flow", "n4js", "typescript"],
+    r: "javascript",
   },
-  'js-templates':
-  {
-    p: ['css', 'css-extras', 'graphql', 'markdown', 'markup'],
-    r: 'javascript'
+  "js-templates": {
+    p: ["css", "css-extras", "graphql", "markdown", "markup"],
+    r: "javascript",
   },
   json: {},
-  jsonp: { r: 'json' },
-  json5: { r: 'json' },
+  jsonp: { r: "json" },
+  json5: { r: "json" },
   julia: {},
   keyman: {},
-  kotlin: { r: 'clike' },
-  latex: { a: ['tex', 'context'] },
-  less: { r: 'css' },
-  lilypond: { r: 'scheme', a: 'ly' },
+  kotlin: { r: "clike" },
+  latex: { a: ["tex", "context"] },
+  less: { r: "css" },
+  lilypond: { r: "scheme", a: "ly" },
   liquid: {},
-  lisp: { a: ['emacs', 'elisp', 'emacs-lisp'] },
+  lisp: { a: ["emacs", "elisp", "emacs-lisp"] },
   livescript: {},
   lolcode: {},
   lua: {},
   makefile: {},
-  markdown: { r: 'markup', a: 'md' },
-  'markup-templating': { r: 'markup' },
+  markdown: { r: "markup", a: "md" },
+  "markup-templating": { r: "markup" },
   matlab: {},
   mel: {},
   mizar: {},
   monkey: {},
   n1ql: {},
-  n4js: { p: ['jsdoc'], r: 'javascript', a: 'n4jsd' },
-  'nand2tetris-hdl': {},
+  n4js: { p: ["jsdoc"], r: "javascript", a: "n4jsd" },
+  "nand2tetris-hdl": {},
   nasm: {},
-  nginx: { r: 'clike' },
+  nginx: { r: "clike" },
   nim: {},
   nix: {},
   nsis: {},
-  objectivec: { r: 'c' },
+  objectivec: { r: "c" },
   ocaml: {},
-  opencl: { p: ['c', 'cpp'], r: 'cpp' },
+  opencl: { p: ["c", "cpp"], r: "cpp" },
   oz: {},
   parigp: {},
-  parser: { r: 'markup' },
-  pascal: { a: 'objectpascal' },
+  parser: { r: "markup" },
+  pascal: { a: "objectpascal" },
   pascaligo: {},
-  pcaxis: { a: 'px' },
+  pcaxis: { a: "px" },
   perl: {},
-  php: { r: ['clike', 'markup-templating'] },
-  phpdoc: { r: ['php', 'javadoclike'] },
-  'php-extras': { r: 'php' },
-  plsql: { r: 'sql' },
+  php: { r: ["clike", "markup-templating"] },
+  phpdoc: { r: ["php", "javadoclike"] },
+  "php-extras": { r: "php" },
+  plsql: { r: "sql" },
   powershell: {},
-  processing: { r: 'clike' },
+  processing: { r: "clike" },
   prolog: {},
   properties: {},
-  protobuf: { r: 'clike' },
-  pug:
-  {
-    p: ['coffeescript', 'ejs', 'handlebars', 'less', 'livescript', 'markdown', 'scss', 'stylus', 'twig'],
-    r: ['markup', 'javascript']
+  protobuf: { r: "clike" },
+  pug: {
+    p: [
+      "coffeescript",
+      "ejs",
+      "handlebars",
+      "less",
+      "livescript",
+      "markdown",
+      "scss",
+      "stylus",
+      "twig",
+    ],
+    r: ["markup", "javascript"],
   },
   puppet: {},
-  pure: { p: ['c', 'cpp', 'fortran'] },
-  python: { a: 'py' },
+  pure: { p: ["c", "cpp", "fortran"] },
+  python: { a: "py" },
   q: {},
-  qore: { r: 'clike' },
+  qore: { r: "clike" },
   r: {},
-  jsx:
-  {
-    p: ['jsdoc', 'js-extras', 'js-templates'],
-    r: ['markup', 'javascript']
+  jsx: {
+    p: ["jsdoc", "js-extras", "js-templates"],
+    r: ["markup", "javascript"],
   },
-  tsx: { r: ['jsx', 'typescript'] },
+  tsx: { r: ["jsx", "typescript"] },
   renpy: {},
-  reason: { r: 'clike' },
-  regex: { p: ['actionscript', 'coffeescript', 'flow', 'javascript', 'typescript', 'vala'] },
+  reason: { r: "clike" },
+  regex: {
+    p: [
+      "actionscript",
+      "coffeescript",
+      "flow",
+      "javascript",
+      "typescript",
+      "vala",
+    ],
+  },
   rest: {},
   rip: {},
   roboconf: {},
-  ruby: { r: 'clike', a: 'rb' },
+  ruby: { r: "clike", a: "rb" },
   rust: {},
   sas: {},
-  sass: { r: 'css' },
-  scss: { r: 'css' },
-  scala: { r: 'java' },
+  sass: { r: "css" },
+  scss: { r: "css" },
+  scala: { r: "java" },
   scheme: {},
-  'shell-session': { r: 'bash' },
+  "shell-session": { r: "bash" },
   smalltalk: {},
-  smarty: { r: 'markup-templating' },
-  soy: { r: 'markup-templating' },
-  'splunk-spl': {},
+  smarty: { r: "markup-templating" },
+  soy: { r: "markup-templating" },
+  "splunk-spl": {},
   sql: {},
   stylus: {},
-  swift: { r: 'clike' },
-  tap: { r: 'yaml' },
+  swift: { r: "clike" },
+  tap: { r: "yaml" },
   tcl: {},
-  textile: { p: 'css', r: 'markup' },
+  textile: { p: "css", r: "markup" },
   toml: {},
-  tt2: { r: ['clike', 'markup-templating'] },
-  twig: { r: 'markup' },
-  typescript: { p: 'js-templates', r: 'javascript', a: 'ts' },
-  't4-cs': { r: ['t4-templating', 'csharp'], a: 't4' },
-  't4-vb': { r: ['t4-templating', 'visual-basic'] },
-  't4-templating': {},
-  vala: { r: 'clike' },
-  vbnet: { r: 'basic' },
-  velocity: { r: 'markup' },
+  tt2: { r: ["clike", "markup-templating"] },
+  twig: { r: "markup" },
+  typescript: { p: "js-templates", r: "javascript", a: "ts" },
+  "t4-cs": { r: ["t4-templating", "csharp"], a: "t4" },
+  "t4-vb": { r: ["t4-templating", "visual-basic"] },
+  "t4-templating": {},
+  vala: { r: "clike" },
+  vbnet: { r: "basic" },
+  velocity: { r: "markup" },
   verilog: {},
   vhdl: {},
   vim: {},
-  'visual-basic': { a: 'vb' },
+  "visual-basic": { a: "vb" },
   wasm: {},
-  wiki: { r: 'markup' },
-  xeora: { r: 'markup', a: 'xeoracube' },
+  wiki: { r: "markup" },
+  xeora: { r: "markup", a: "xeoracube" },
   xojo: {},
-  xquery: { r: 'markup' },
-  yaml: { a: 'yml' }
+  xquery: { r: "markup" },
+  yaml: { a: "yml" },
 };
 
 export async function loadLang(ext: string) {
@@ -238,21 +260,29 @@ export async function loadLang(ext: string) {
     const { a: alias, r: require } = langData[item];
     if (alias && (Array.isArray(alias) ? alias : [alias]).includes(ext)) {
       if (require) {
-        langs = Array.from(new Set([...langs, ...(Array.isArray(require) ? require : [require])]));
+        langs = Array.from(
+          new Set([...langs, ...(Array.isArray(require) ? require : [require])])
+        );
       }
     }
     if (ext === item) {
       if (require) {
-        langs = Array.from(new Set([...langs, ...(Array.isArray(require) ? require : [require])]));
+        langs = Array.from(
+          new Set([...langs, ...(Array.isArray(require) ? require : [require])])
+        );
       }
       langs.push(ext);
     }
   });
   try {
     if (langs.length > 0) {
-      return Promise.all(langs.map((key) => import(`prismjs/components/prism-${key}`)));
+      return Promise.all(
+        langs.map((key) => import(`prismjs/components/prism-${key}`))
+      );
     }
-  } catch (error) { }
+  } catch (error) {
+    console.error("failed to import", langs);
+  }
 }
 
 export default langData;
